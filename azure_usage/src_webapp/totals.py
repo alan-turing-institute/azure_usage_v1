624570220c61d8513f8e3f4d58aef0f9ba13e34e
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-05-04 21:25:40 +0100

    Fixing minor bugs and cleanup

--- azure_usage/src_webapp/totals.py
+++ azure_usage/src_webapp/totals.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
 """
 Python module to perform operations related to aggregated numbers.
-
-Author: Tomas Lazauskas
 """
 
 import pandas as pd
-import numpy as np
 
-from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 from .constants import (
     CONST_COL_NAME_SGUID,
     CONST_COL_NAME_YM,
     CONST_COL_NAME_DATE,
-    CONST_COL_NAME_COST
+    CONST_COL_NAME_COST,
 )
+
 
 def group_day(raw_data, add_missing_days=False):
     """Groups raw usage by calender day
@@ -30,13 +27,27 @@ def group_day(raw_data, add_missing_days=False):
 
     raw_data_ = raw_data.copy()
 
-    raw_data_gr = raw_data_.groupby([CONST_COL_NAME_DATE])[CONST_COL_NAME_COST].sum().reset_index().sort_values(by=[CONST_COL_NAME_DATE], ascending=True)
+    raw_data_gr = (
+        raw_data_.groupby([CONST_COL_NAME_DATE])[CONST_COL_NAME_COST]
+        .sum()
+        .reset_index()
+        .sort_values(by=[CONST_COL_NAME_DATE], ascending=True)
+    )
 
     if add_missing_days:
         raw_data_gr.set_index(CONST_COL_NAME_DATE, inplace=True)
-        raw_data_gr = raw_data_gr.resample('D').asfreq().fillna(0).reset_index()
+        raw_data_gr = raw_data_gr.resample("D").asfreq().fillna(0).reset_index()
+
+    raw_data_gr["Date"] = pd.to_datetime(raw_data_gr["Date"])
+
+    raw_data_gr["Date_str"] = raw_data_gr["Date"].dt.strftime("%Y-%m-%d")
+
+    # raw_data_gr['Date_id'] = [x for x in range(raw_data_gr.shape[0])]
+
+    raw_data_gr.reset_index(drop=True, inplace=True)
 
     return raw_data_gr
+
 
 def add_missing_year_months(df, date_from, date_to, value_col_name):
     """Adds missing Year-Month values from the given range to the dataframe with zero values
@@ -55,13 +66,18 @@ def add_missing_year_months(df, date_from, date_to, value_col_name):
     cur_date = date_from
 
     while cur_date < date_to:
-        year_month = '{year}-{month:02d}'.format(year=cur_date.year, month=cur_date.month)
+        year_month = "{year}-{month:02d}".format(
+            year=cur_date.year, month=cur_date.month
+        )
         cur_date += relativedelta(months=1)
 
-        if not (year_month in df[CONST_COL_NAME_YM].values):
-            df = df.append({CONST_COL_NAME_YM: year_month, value_col_name:0.0}, ignore_index=True)
+        if not year_month in df[CONST_COL_NAME_YM].values:
+            df = df.append(
+                {CONST_COL_NAME_YM: year_month, value_col_name: 0.0}, ignore_index=True
+            )
 
     return df
+
 
 def group_year_month(raw_data):
     """Groups raw usage by calender month
@@ -73,11 +89,19 @@ def group_year_month(raw_data):
     """
 
     raw_data_ = raw_data.copy()
-    raw_data_[CONST_COL_NAME_YM] = pd.to_datetime(raw_data_[CONST_COL_NAME_DATE]).apply(lambda x: '{year}-{month:02d}'.format(year=x.year, month=x.month))
+    raw_data_[CONST_COL_NAME_YM] = pd.to_datetime(raw_data_[CONST_COL_NAME_DATE]).apply(
+        lambda x: "{year}-{month:02d}".format(year=x.year, month=x.month)
+    )
 
-    raw_data_gr = raw_data_.groupby([CONST_COL_NAME_YM])[CONST_COL_NAME_COST].sum().reset_index().sort_values(by=[CONST_COL_NAME_YM], ascending=True)
+    raw_data_gr = (
+        raw_data_.groupby([CONST_COL_NAME_YM])[CONST_COL_NAME_COST]
+        .sum()
+        .reset_index()
+        .sort_values(by=[CONST_COL_NAME_YM], ascending=True)
+    )
 
     return raw_data_gr
+
 
 def group_sub_year_month(raw_data):
     """Groups raw usage by calender month and subscriptionid
@@ -89,10 +113,17 @@ def group_sub_year_month(raw_data):
     """
 
     raw_data_ = raw_data.copy()
-    raw_data_[CONST_COL_NAME_YM] = pd.to_datetime(raw_data_[CONST_COL_NAME_DATE]).apply(lambda x: '{year}-{month:02d}'.format(year=x.year, month=x.month))
+    raw_data_[CONST_COL_NAME_YM] = pd.to_datetime(raw_data_[CONST_COL_NAME_DATE]).apply(
+        lambda x: "{year}-{month:02d}".format(year=x.year, month=x.month)
+    )
 
-    raw_data_gr = raw_data_.groupby([CONST_COL_NAME_SGUID, CONST_COL_NAME_YM])[CONST_COL_NAME_COST].sum().reset_index().sort_values(by=[CONST_COL_NAME_YM], ascending=True)
+    raw_data_gr = (
+        raw_data_.groupby([CONST_COL_NAME_SGUID, CONST_COL_NAME_YM])[
+            CONST_COL_NAME_COST
+        ]
+        .sum()
+        .reset_index()
+        .sort_values(by=[CONST_COL_NAME_YM], ascending=True)
+    )
 
     return raw_data_gr
-
-
